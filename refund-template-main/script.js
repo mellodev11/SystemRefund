8cf08e39d2884cc5657684a91cb478ee9413c04e
mellodev11 <robertale45@gmail.com>
2024-08-27 22:45:49 -0300

    Formatando a moeda

--- refund-template-main/script.js
+++ refund-template-main/script.js
@@ -4,7 +4,16 @@ const amount  = document.getElementById("amount")
 
 amount.oninput = () =>{
   let value = amount.value.replace(/\D/g, "")
-  amount.value = value
-
+  
+  value = Number(value) / 100
+  
+  amount.value = formtCurrencyBRL(value) 
 }
 
+function formtCurrencyBRL(value){
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  } )
+  return value
+}
