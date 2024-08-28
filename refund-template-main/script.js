faaf3d375aeef8db88cc9d8ce19d4d8581645bff
mellodev11 <robertale45@gmail.com>
2024-08-28 19:13:22 -0300

    Criando icone da categoria despesa

--- refund-template-main/script.js
+++ refund-template-main/script.js
@@ -2,6 +2,7 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 const form = document.querySelector("form");
+const expenseList = document.querySelector("ul");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -30,13 +31,19 @@ form.onsubmit = (e) => {
     amount: amount.value,
     create_at: new Date(),
   };
-  expensesAdd(newExpense);
+  expenseAdd(newExpense);
 };
 
-function expensesAdd(newExpense) {
+function expenseAdd(newExpense) {
   try {
     const expenseItem = document.createElement("li");
     expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+    expenseItem.append(expenseIcon);
+    expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível adicionar a despesa");
     console.log(error);
