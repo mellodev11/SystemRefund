9255e86b34d2451f29f4c37ddf575bc862be84ea
mellodev11 <robertale45@gmail.com>
2024-08-28 19:29:05 -0300

    Criando a Informação da despesa

--- refund-template-main/script.js
+++ refund-template-main/script.js
@@ -42,7 +42,18 @@ function expenseAdd(newExpense) {
     const expenseIcon = document.createElement("img");
     expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
     expenseIcon.setAttribute("alt", newExpense.category_name);
-    expenseItem.append(expenseIcon);
+
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    expenseInfo.append(expenseName, expenseCategory);
+    expenseItem.append(expenseIcon, expenseInfo);
     expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível adicionar a despesa");
